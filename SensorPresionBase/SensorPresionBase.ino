9f10aff4f0b9b6b97017e20807cb53decef583ed
Marc <marcvalentinesmiro@gmail.com>
2025-05-13 10:27:21 +0200

    Verificar si se detecta el sensor
    Modificar la dirección I2C por la real

--- SensorPresionBase/SensorPresionBase.ino
+++ SensorPresionBase/SensorPresionBase.ino
@@ -4,11 +4,11 @@
 
 //leds
 #define LED_PIN 6
-#define NUM_LEDS 19
+#define NUM_LEDS 64
 #define LED_TYPE SK6812
 #define COLOR_ORDER GRB
 
-CGRB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 
 //sensor
 Adafruit_BMP280 bmp;
@@ -26,29 +26,32 @@ void setup() {
   FastLED.show();
   //Sensor
   delay(500);
-  presionBase = bmp.readPreassure() / 100; // en hPa
+  presionBase = bmp.readPressure() / 100; // en hPa
+  bool sensorDisponible = bmp.begin(0x76); //dirección I2C de ejemplo (sustituir por la real)
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  unsigned float presion = bmp.readPressure() / 100;
+  if(sensorDisponible){
+    unsigned float presion = bmp.readPressure() / 100;
 
-  //calcular diferéncia respecto a la presión base
-  unsigned float dif = presionBase - presion;
+    //calcular diferéncia respecto a la presión base
+    unsigned float dif = presionBase - presion;
 
-  //mapear el cambio de leds según la presión
-  din = constrain(dif, 0, 5); //limitar el valor para evitar errores
-  unsigned int maxEncender = map(dif * 100, 0, 500, 0, NUM_LEDS); //multiplicar por 100 porque map() requiere enteros
+    //mapear el cambio de leds según la presión
+    din = constrain(dif, 0, 5); //limitar el valor para evitar errores
+    unsigned int maxEncender = map(dif * 100, 0, 500, 0, NUM_LEDS); //multiplicar por 100 porque map() requiere enteros
 
-  if(index < maxEncender && index < NUM_LEDS){
-    uint8_t tono = map(index, 0, NUM_LEDS -1, 0, 96);
-    leds[index] = CHSV(tono, 255, 255);
-    index++;
-  } else if (index > maxEncender && index > 0){
-    leds[index] = CRGB::Black;
-    index--;
+    if(index < maxEncender && index < NUM_LEDS){
+      uint8_t tono = map(index, 0, NUM_LEDS -1, 0, 96);
+      leds[index] = CHSV(tono, 255, 255);
+      index++;
+    } else if (index > maxEncender && index > 0){
+      leds[index] = CRGB::Black;
+      index--;
+    }
+
+    FastLED.show();
+    delay(800);
   }
-
-  FastLED.show();
-  delay(1000);
 }
